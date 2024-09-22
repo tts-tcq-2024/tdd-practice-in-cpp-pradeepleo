cb0f1e1b00a6cca1a0d86b56214ad167c1c630e9
Pradeep_03 <51703548+pradeepleo@users.noreply.github.com>
2024-09-22 22:44:49 +0530

    Update StringCalculator.h

--- StringCalculator.h
+++ StringCalculator.h
@@ -1,9 +1,8 @@
 #ifndef STRINGCALCULATOR_H
 #define STRINGCALCULATOR_H
+#include <string>
 
-class StringCalculator {
-public:
     int CalculateAdd(const std::string &input); 
-};
+
 
 #endif // STRINGCALCULATOR_H
